2e773e441fa81425139126d78e1ba68fadafdce8
Josh Mitchell <jmitchell@rand.com>
2018-01-16 17:13:39 -0500

    initial

---
+++ PowershellNetcoreDemo2/Program.fs
@@ -0,0 +1,17 @@
+﻿
+open System
+open System.Management.Automation
+
+let PsVersion = 
+    use ps = PowerShell.Create()
+    ps.AddScript("$PSVersionTable.PSVersion").Invoke().Item(0).ToString()
+
+    
+
+[<EntryPoint>]
+let main _ =
+    printfn "The PowerShell version is: %s" PsVersion
+
+    printfn "\nPress any key to exit."
+    Console.Read() |> ignore
+    0 // return an integer exit code
