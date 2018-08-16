5142b1c1c2fc437269bb9c65acc28cac54caf2f9
Josh Mitchell <jmitchell@rand.com>
2018-08-16 16:55:01 -0400

    C# embedded in PS

---
+++ ps-csembed/example2.ps1
@@ -0,0 +1,27 @@
+$cs = @"
+using System;
+using static System.Math;
+
+public class Calc
+{
+    public static int RandomInt() => (new Random()).Next();
+
+    //Span<int> arr = stackalloc [] {1, 2, 3};
+
+    public void WriteTuple()
+    {
+        var tuple = (A: "one", B: "two");
+        Console.WriteLine(tuple.A);
+        Console.WriteLine(tuple.B);
+    }
+}
+"@
+
+Add-Type -TypeDefinition $cs -Language CSharp # -ReferencedAssemblies $assemblies
+
+[Calc]::RandomInt()
+
+$calc = New-Object Calc 
+$calc.WriteTuple()
+
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/add-type?view=powershell-6
