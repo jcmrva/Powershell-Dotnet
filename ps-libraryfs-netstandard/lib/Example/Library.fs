ce9e25ef431c93832496f9e167c18f8cac5a7692
Josh M <20140997+jcmrva@users.noreply.github.com>
2018-08-20 23:38:11 -0400

    fs lib example

--- ps-libraryfs-netstandard/lib/Example/Library.fs
+++ ps-libraryfs-netstandard/lib/Example/Library.fs
@@ -1,5 +1,12 @@
 namespace Example
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+
+type ClassName(name: string) =
+
+    member __.Greeting() = printfn "Hello %s" name
+
+    static member FsHello = "Hello from F#"
+
+module Math = 
+    
+    let cube x = x * x * x
