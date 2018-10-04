cf0cb80c719a4af1a19f86a889da32416d6175f2
Josh Mitchell <20140997+jcmrva@users.noreply.github.com>
2018-10-04 14:11:41 -0400

    cs-psprocess

---
+++ cs-psprocess-netcoreapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base folder: " + AppContext.BaseDirectory);
+
+            var pinfo = new ProcessStartInfo();
+            pinfo.FileName = "powershell.exe";
+            pinfo.Arguments = "-File example.ps1";
+            pinfo.CreateNoWindow = true;
+            pinfo.UseShellExecute = false;
+            pinfo.RedirectStandardOutput = true;
+
+            using (Process proc = new Process() { StartInfo = pinfo, EnableRaisingEvents = true })
+            {
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    string line = proc.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                proc.WaitForExit();
+                Console.WriteLine(proc.ExitCode);
+            }
+        }
+    }
+}
