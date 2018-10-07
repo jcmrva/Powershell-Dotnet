e008d8be505ca19fd737cc112b2efb7d1cd74e8a
Josh M <20140997+jcmrva@users.noreply.github.com>
2018-10-07 16:18:35 -0400

    started fs-psprocess-netcoreapp

---
+++ fs-psprocess-netcoreapp/Program.fs
@@ -0,0 +1,25 @@
+﻿
+//open System
+open System.Diagnostics
+
+[<EntryPoint>]
+let main _ =
+    
+    let pinfo = ProcessStartInfo();
+    pinfo.FileName <- "powershell.exe";
+    pinfo.Arguments <- "-File example.ps1 -MyParam \"Hello to PS from C# and back again.\" -Test";
+    pinfo.CreateNoWindow <- true;
+    pinfo.UseShellExecute <- false;
+    pinfo.RedirectStandardOutput <- true;
+
+    use proc = new Process()
+    proc.StartInfo <- pinfo
+    proc.EnableRaisingEvents <- true
+
+    let started = proc.Start()
+    if not started then printfn "Process failed to start."
+
+
+    proc.WaitForExit()
+
+    0
